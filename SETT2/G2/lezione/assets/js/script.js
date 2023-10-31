61fac5de8551ae981aa52053d48fd5a5cc4ad9bc
lindaweb92 <148793222+lindaweb92@users.noreply.github.com>
2023-10-31 12:58:35 +0100

    Sett2 G2 lezione mattina

---
+++ SETT2/G2/lezione/assets/js/script.js
@@ -0,0 +1,30 @@
+// let mioOggetto = {};
+
+// letmioArray = [];
+
+const mioOggetto = {
+    nome: 'Linda',
+    cognome:'Breveglieri',
+    eta: 31,
+    studente: true,
+}
+document.getElementById('name').innerHTML = mioOggetto.name;
+document.getElementById('surname').innerHTML = mioOggetto.surname;
+document.getElementById('eta').innerHTML = mioOggetto.eta;
+
+mioOggetto.email = 'breveglieri@gmail.com';
+document.getElementById('email').innerHTML = mioOggetto.eta;
+
+
+
+
+
+document.getElementById('oggetto').innerHTML = mioOggetto.cognome;
+
+mioOggetto.email = 'breveglieri@gmail.com';
+document.getElementById('oggetto').innerHTML +- = '' + mioOggetto.email;
+
+
+let variabile1 = 'Pippo';
+let variabile2 = 'variabile';
+console.log()
