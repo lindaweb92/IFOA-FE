3e60fc1c750208b5c26ea536242ce0e246d54cd6
lindaweb92 <148793222+lindaweb92@users.noreply.github.com>
2024-01-19 12:08:27 +0100

    PROGETTO SETT 10 - G5 - WEATHER APP REACT

---
+++ SETT10/G5/weather/weather/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "21efcd4989131d7914087360ad224c2a",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
